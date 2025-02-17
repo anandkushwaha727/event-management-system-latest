a741ad5e343030b7b96444d09951ff0d1d1360b5
Anand Kushwaha <anandkushwaha727@gmail.com>
2025-02-17 17:13:19 +0530

    Added CORS for frontend URL

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 // Attach WebSocket to the same server  
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://event-managementpuce.vercel.app",
         methods: ["GET", "POST"]
     }
 });
